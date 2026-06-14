281735650ee5c0ef6e741ee5405224b5bd09b1d7
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2026-06-14 16:26:47 +0530

    updated .html templates for better experience, understanding and comparsion

--- src/xss.rs
+++ src/xss.rs
@@ -4,19 +4,18 @@ use axum::{
 };
 use std::collections::HashMap;
 
-pub async fn xss(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+pub async fn xss() -> Html<&'static str> {
+    Html(include_str!("../templates/xss.html"))
+}
+
+pub async fn reflect_xss(Query(params): Query<HashMap<String, String>>) -> Html<String> {
     let input = params.get("q").map(String::as_str).unwrap_or("");
     Html(format!(
         r#"
         <html>
-            <body>
-                <h1>reflected xss demo</h1>
-                <p>you searched for: {}</p>
-
-                <form action="/xss">
-                    <input type="text" name="q">
-                    <button type="submit">search</button>
-                </form>
+            <body style="font-family: sans-serif; padding: 10px; background: #fff;">
+                <h3>Reflected Output:</h3>
+                <div>{}</div>
             </body>
         </html>
         "#,
--- src/xssfixed.rs
+++ src/xssfixed.rs
@@ -5,20 +5,15 @@ use axum::{
 use std::collections::HashMap;
 use html_escape::encode_text;
 
-pub async fn xss_safe(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+pub async fn reflect_xss_safe(Query(params): Query<HashMap<String, String>>) -> Html<String> {
     let input = params.get("q").map(String::as_str).unwrap_or("");
     let safe_input = encode_text(input);
     Html(format!(
         r#"
         <html>
-            <body>
-                <h1>no reflected xss</h1>
-                <p>you searched for: {}</p>
-
-                <form action="/xss-safe">
-                    <input type="text" name="q">
-                    <button type="submit">search</button>
-                </form>
+            <body style="font-family: sans-serif; padding: 10px; background: #fff;">
+                <h3>Reflected Output:</h3>
+                <div>{}</div>
             </body>
         </html>
         "#,
